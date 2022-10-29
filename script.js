cf6dad0ab2c76b41716a45e789f5f3da72811776
Van Phan <win1852000@gmail.com>
2022-10-29 17:42:54 -0400

    add js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log("Hello Hello")
+let randomNumber = Math.floor(Math.random()*3)
+console.log(randomNumber)
+
+if (randomNumber === 0){
+    let randomNumber = "rock"
+    console.log(randomNumber)
+} else if(randomNumber === 1){
+    let randomNumber = "paper"
+    console.log(randomNumber)
+} else{
+    let randomNumber = "scissor"
+    console.log(randomNumber)
+}
+
+function rock() {
+    if (randomNumber === 2 ){
+        console.log("you win,rock beat scissor")
+    } else if(randomNumber === 1){
+        console.log("you lose, paper beat rock")
+       
+    } else {
+        console.log("It's a tie")
+    }
+}
+
+function paper() {
+    if (randomNumber === 2){
+        console.log("you lose,scissor beat paper")
+    } else if(randomNumber === 0){
+        console.log("you win,paper beat rock")
+    } else{
+        console.log("It's a tie")
+    }
+
+}
+
+function scissor() {
+    if (randomNumber === 0){
+        console.log("you lose")
+        console.log("rock beat scissor")
+    }else if (randomNumber===1){
+        console.log("you win, scissor beat paper")
+    }else {
+        console.log("It's a tie")
+    }
+}
+
+let myPoints = document.getElementById("4").innerText= "adasd"
+
+
+
+
+
+
+
